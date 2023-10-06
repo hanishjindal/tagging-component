0012b557308575cf59c1e9abe603ba4f51c9a540
Hanish Jindal <hanish1139.cse19@chitkara.edu.in>
2023-10-06 10:52:21 +0530

    first commit

---
+++ app/config.js
@@ -0,0 +1,16 @@
+export const IMAGE_LINKS = [
+    'https://images.pexels.com/photos/977003/pexels-photo-977003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1213294/pexels-photo-1213294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/38637/car-audi-auto-automotive-38637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/38637/car-audi-auto-automotive-38637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/303316/pexels-photo-303316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/303316/pexels-photo-303316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/4635345/pexels-photo-4635345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/12393014/pexels-photo-12393014.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/12393014/pexels-photo-12393014.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/2475808/pexels-photo-2475808.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/3954422/pexels-photo-3954422.png?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/11001096/pexels-photo-11001096.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/11001096/pexels-photo-11001096.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    'https://images.pexels.com/photos/9846101/pexels-photo-9846101.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+]
--- app/page.js
+++ app/page.js
@@ -1,113 +1,76 @@
-import Image from 'next/image'
+'use client';
+
+import Image from 'next/image';
+import { IMAGE_LINKS } from './config';
+import { useState, useRef } from 'react';
 
 export default function Home() {
+  const [imageIdx, setImageIdx] = useState(0);
+  const [messageBoxCoords, setMessageBoxCoords] = useState([]);
+
+  const ref = useRef(null)
+
+  const handleImageClick = (event) => {
+    // Calculate the coordinates relative to the image
+    const image = event.target;
+    console.log((window.innerWidth - ref.current.offsetWidth), window.innerWidth, ref.current.offsetWidth, event.clientX)
+    const x = (window.innerWidth - ref.current.offsetWidth) + event.clientX - 20;
+    const y = (window.innerHeight - ref.current.offsetHeight) + event.clientY + 20;
+
+    // console.log(event.clientX)
+
+    // Add the coordinates to the array
+    setMessageBoxCoords((prevCoords) => [...prevCoords, { x, y }]);
+  };
+
+  const handleDragEnd = (event, index) => {
+    const coords = structuredClone(messageBoxCoords)
+
+    const newX = event.clientX
+    const newY = event.clientY
+
+    const diffMinX = window.innerWidth - ref.current.offsetWidth
+    const diffMinY = window.innerHeight - ref.current.offsetHeight
+
+    const diffMaxX = window.innerWidth - diffMinX
+    const diffMaxY = window.innerHeight - diffMinY
+    coords[index] = {
+      x: (newX > diffMinX) ? ((newX < diffMaxX) ? newX : ref.current.offsetWidth) : diffMinX,
+      y: (newY > diffMinY) ? ((newY < diffMaxY) ? newY : ref.current.offsetHeight) : diffMinY
+    }
+    setMessageBoxCoords(coords)
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='w-screen h-screen flex-col flex items-center p-10 px-20 gap-5 select-none'>
+      <h1 className='text-2xl font-bold'>Tagging Component</h1>
+      <div className='flex relative w-full h-[90%] bg-gray-200 border border-gray-500 rounded p-5 justify-center items-center cursor-grabbing'>
+        <div className='w-fit h-full'>
+          <img
+            src={IMAGE_LINKS[imageIdx]}
+            alt='image'
+            className='h-full'
+            onDoubleClick={(e) => handleImageClick(e)}
+            ref={ref}
+          />
+          {messageBoxCoords.map((coords, index) => (
+            <div
+              key={index}
+              className='absolute bg-white border border-gray-500 p-2'
+              draggable={true}
+              onDragEndCapture={(e) => handleDragEnd(e, index)}
+              style={{
+                position: 'absolute',
+                left: (coords.x - (window.innerWidth - ref.current.offsetWidth)),
+                top: (coords.y - (window.innerHeight - ref.current.offsetHeight)),
+              }}
+            >
+              Your message here {index + 1}
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
